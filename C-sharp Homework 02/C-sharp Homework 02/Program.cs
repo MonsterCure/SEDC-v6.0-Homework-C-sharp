24da1ec3dba6c96f3718138acbac6a4dde47a8d8
MonsterCure <mm13586@gmail.com>
2018-03-16 17:03:58 +0100

    Add files via upload

---
+++ C-sharp Homework 02/C-sharp Homework 02/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Homework_02
+{
+    class Program
+    {
+        static void Man()
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  * * *");
+            Console.WriteLine(" *  *  *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+        }
+
+        static void JumpingMan()
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine("*   *   *");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("   * *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+        }
+
+        static void Main(string[] args)
+        {          
+            do
+            {
+                Man();
+
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+
+                JumpingMan();
+
+                System.Threading.Thread.Sleep(500);
+                Console.Clear();
+
+            } while (Console.KeyAvailable == false);
+        }
+    }
+}
